625a89836e7081f8831803b1d9a0dbe8e5a65e71
mattcs2401 <mattcs2401@gmail.com>
2016-11-09 21:00:11 +1000

    Minor update.

--- app/src/main/java/com/mcssoft/racemeeting/interfaces/IEditMeeting.java
+++ app/src/main/java/com/mcssoft/racemeeting/interfaces/IEditMeeting.java
@@ -2,9 +2,11 @@ package com.mcssoft.racemeeting.interfaces;
 
 public interface IEditMeeting {
     /**
-     * Replace the ListingFragment with the EditFragment.
+     * Replace the ListingFragment with the EditFragment (called in MainActivity).
+     * Replace the DetailFragment with the EditFragment (called in DetailActivity)
      * @param dbRowId The database row id corredsponding to the selected item in the listing, or a
-     *                value of 0 to indicate a new meeting..
+     *                value of 0 to indicate a new meeting.
+     *  Bundle extras also provided to indicate editing an existing entry, or a new one.
      */
     void onEditMeeting(long dbRowId);
 }
