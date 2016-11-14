60b9fdede2e93c9633ea427b94302a7cb3850737
mattcs2401 <mattcs2401@gmail.com>
2016-11-14 12:43:45 +1000

    Add in new listener interfaces.

---
+++ app/src/main/java/com/mcssoft/racemeeting/listener/MeetingClickListener.java
@@ -0,0 +1,7 @@
+package com.mcssoft.racemeeting.listener;
+
+import android.view.View;
+
+public interface MeetingClickListener {
+    public void onItemClick(View view, int position);
+}
---
+++ app/src/main/java/com/mcssoft/racemeeting/listener/MeetingLongClickListener.java
@@ -0,0 +1,7 @@
+package com.mcssoft.racemeeting.listener;
+
+import android.view.View;
+
+public interface MeetingLongClickListener {
+    public void onItemLongClick(View view, int position);
+}
